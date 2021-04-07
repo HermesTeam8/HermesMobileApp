94778f3cc7a487d324b00c09806c9489a89bebe2
cindyherrera <herreracindy9@gmail.com>
2021-04-07 15:03:24 -0400

    Initial commit

---
+++ app/src/main/java/com/example/hermes/scrollviewss.java
@@ -0,0 +1,14 @@
+package com.example.hermes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scrollviewss extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrollviewss);
+    }
+}
