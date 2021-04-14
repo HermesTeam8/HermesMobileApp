d564c26f472146e8c3b6c651c6d4920a44c6ce28
cindyherrera <herreracindy9@gmail.com>
2021-04-14 04:01:37 -0400

    navigation view edits

--- app/src/main/java/com/example/hermes/MainActivity.java
+++ app/src/main/java/com/example/hermes/MainActivity.java
@@ -1,24 +1,94 @@
 package com.example.hermes;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.navigation.NavigationView;
+
+//import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-    private Button login;
+
+    private NavigationView navigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+    private RecyclerView messageList;
+    private Toolbar mToolbar;
+
+
+    //private Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        login = (Button) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        //setSupportActionBar(mToolbar);
+        //getSupportActionBar().setTitle("HOME PAGE");
+        mToolbar.setTitle("HOMEPAGE!!");
+        drawerLayout = (DrawerLayout)   findViewById(R.id.drawable_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.drawer_open,R.string.drawer__close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView = (NavigationView)   findViewById(R.id.navigation_view);
+
+        View navView = navigationView.inflateHeaderView(R.layout.navigation_header);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View v) {
-                openActivityLogin();
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                UserMenuSelector(item);
+                return false;
             }
+
+
         });
+//        login = (Button) findViewById(R.id.login);
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openActivityLogin();
+//            }
+//        });
     }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void UserMenuSelector(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Toast.makeText(this,"HOME", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_connections:
+                Toast.makeText(this,"Connections", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_messages:
+                Toast.makeText(this,"Messages", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this,"Settings", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
     public void openActivityLogin() {
         Intent intent = new Intent(this, activity_login.class);
         startActivity(intent);
